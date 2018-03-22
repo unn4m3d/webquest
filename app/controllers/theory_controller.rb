0431d1ef0b033c02b1c832d18d49448f7c537b93
unn4m3d <smelnikov871@gmail.com>
2018-03-22 20:10:35 +1000

    Some theory

--- app/controllers/theory_controller.rb
+++ app/controllers/theory_controller.rb
@@ -4,6 +4,21 @@ class TheoryController < ApplicationController
   end
 
   def show
+    prv = params[:page].to_i - 1
+    nxt = params[:page].to_i + 1
+
+    if prv > 0
+      @previous = prv
+    else
+      @previous = nil
+    end
+
+    if TheoryHelper.pages.include? nxt.to_s
+      @next = nxt
+    else
+      @next = nil
+    end
+
     render "theory/pages/#{params[:page]}"
   end
 end
--- app/helpers/theory_helper.rb
+++ app/helpers/theory_helper.rb
@@ -2,6 +2,6 @@ module TheoryHelper
   DIR = "#{__dir__}/../views/theory/pages"
 
   def self.pages
-    Dir.entries(DIR).select{ |x| !File.directory?(File.join(DIR, x)) }.map{ |x| x.partition('.').first }
+    Dir.entries(DIR).select{ |x| !File.directory?(File.join(DIR, x)) }.map{ |x| x.partition('.').first }.sort_by(&:to_i)
   end
 end
